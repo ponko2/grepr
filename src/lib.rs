753ffa0ea3e92de3141d85bdfd2d57a66cd7a56a
Takahito Nakano <kano@ponko2.jp>
2024-01-26 21:16:56 +0900

    Chapter 9.2.1

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,55 @@
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 use clap::Parser;
+use regex::{Regex, RegexBuilder};
 
 #[derive(Debug, Parser)]
 #[command(version, about, long_about = None)]
-pub struct Args {}
+pub struct Args {
+    #[arg(help = "Search pattern")]
+    pattern: String,
 
-pub fn get_args() -> Result<Args> {
-    Ok(Args::parse())
+    #[arg(value_name = "FILE", help = "Input file(s)", default_value = "-")]
+    files: Vec<String>,
+
+    #[arg(short, long, help = "Case insensitive")]
+    insensitive: bool,
+
+    #[arg(short, long, help = "Recursive search")]
+    recursive: bool,
+
+    #[arg(short, long, help = "Count occurrences")]
+    count: bool,
+
+    #[arg(short = 'v', long, help = "Invert match")]
+    invert_match: bool,
 }
 
-pub fn run(args: Args) -> Result<()> {
-    dbg!(args);
+#[derive(Debug)]
+pub struct Config {
+    pattern: Regex,
+    files: Vec<String>,
+    recursive: bool,
+    count: bool,
+    invert_match: bool,
+}
+
+pub fn get_args() -> Result<Config> {
+    let args = Args::parse();
+    let pattern = &args.pattern;
+    let pattern = RegexBuilder::new(pattern)
+        .case_insensitive(args.insensitive)
+        .build()
+        .map_err(|_| anyhow!("Invalid pattern \"{pattern}\""))?;
+    Ok(Config {
+        pattern,
+        files: args.files,
+        recursive: args.recursive,
+        count: args.count,
+        invert_match: args.invert_match,
+    })
+}
+
+pub fn run(config: Config) -> Result<()> {
+    dbg!(config);
     Ok(())
 }
