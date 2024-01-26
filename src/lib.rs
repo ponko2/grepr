ac0541620f7396009af32ae870767e35cebc396a
Takahito Nakano <kano@ponko2.jp>
2024-01-26 23:37:34 +0900

    Chapter 9.2.3

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,10 @@
 use anyhow::{anyhow, Result};
 use clap::Parser;
 use regex::{Regex, RegexBuilder};
+use std::{
+    fs::File,
+    io::{self, BufRead, BufReader},
+};
 
 #[derive(Debug, Parser)]
 #[command(version, about, long_about = None)]
@@ -56,10 +60,29 @@ pub fn run(config: Config) -> Result<()> {
     for entry in entries {
         match entry {
             Err(err) => eprintln!("{err}"),
-            Ok(filename) => println!("file \"{filename}\""),
+            Ok(filename) => match open(&filename) {
+                Err(err) => eprintln!("{filename}: {err}"),
+                Ok(file) => {
+                    println!(
+                        "Found {:?}",
+                        find_lines(file, &config.pattern, config.invert_match)
+                    );
+                }
+            },
         }
     }
     Ok(())
+}
+
+fn open(filename: &str) -> Result<Box<dyn BufRead>> {
+    Ok(match filename {
+        "-" => Box::new(BufReader::new(io::stdin())),
+        _ => Box::new(BufReader::new(File::open(filename)?)),
+    })
+}
+
+fn find_lines<T: BufRead>(mut file: T, pattern: &Regex, invert_match: bool) -> Result<Vec<String>> {
+    todo!()
 }
 
 fn find_files(paths: &[String], recursive: bool) -> Vec<Result<String>> {
@@ -68,8 +91,11 @@ fn find_files(paths: &[String], recursive: bool) -> Vec<Result<String>> {
 
 #[cfg(test)]
 mod tests {
-    use super::find_files;
+    use std::io::Cursor;
+
+    use super::{find_files, find_lines};
     use rand::{distributions::Alphanumeric, Rng};
+    use regex::{Regex, RegexBuilder};
 
     #[test]
     fn test_find_files() {
@@ -114,5 +140,37 @@ mod tests {
         let files = find_files(&[bad], false);
         assert_eq!(files.len(), 1);
         assert!(files[0].is_err());
+    }
+
+    #[test]
+    fn test_find_lines() {
+        let text = b"Lorem\nIpsum\r\nDOLOR";
+
+        // 「or」というパターンは「Lorem」という1行にマッチするはず
+        let re1 = Regex::new("or").unwrap();
+        let matches = find_lines(Cursor::new(&text), &re1, false);
+        assert!(matches.is_ok());
+        assert_eq!(matches.unwrap().len(), 1);
+
+        // マッチを反転させた場合、残りの2行にマッチするはず
+        let matches = find_lines(Cursor::new(&text), &re1, true);
+        assert!(matches.is_ok());
+        assert_eq!(matches.unwrap().len(), 2);
+
+        // 大文字と小文字を区別しない正規表現
+        let re2 = RegexBuilder::new("or")
+            .case_insensitive(true)
+            .build()
+            .unwrap();
+
+        // 「Lorem」と「DOLOR」の2行にマッチするはず
+        let matches = find_lines(Cursor::new(&text), &re2, false);
+        assert!(matches.is_ok());
+        assert_eq!(matches.unwrap().len(), 2);
+
+        // マッチを反転させた場合、残りの1行にマッチするはず
+        let matches = find_lines(Cursor::new(&text), &re2, true);
+        assert!(matches.is_ok());
+        assert_eq!(matches.unwrap().len(), 1);
     }
 }
